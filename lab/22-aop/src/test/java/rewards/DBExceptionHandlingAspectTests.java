85fc24f7bc0b57d1658a31bae4ddf6aaaeaa84e6
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-08-18 17:48:41 +0300

    Solve optional 22-aop todos.

--- lab/22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
+++ lab/22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
@@ -15,35 +15,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { DbExceptionTestConfig.class })
 @EnableAutoConfiguration
 public class DBExceptionHandlingAspectTests {
 
-    @Autowired
-    AccountRepository repository;
+	@Autowired
+	AccountRepository repository;
 
-    @Test
-    @CaptureSystemOutput
-    public void testReportException(OutputCapture capture) {
-    	
-    	// The repository.findByCreditCard(..) method below will 
-    	// result in an exception because we are using empty database
-    	// set by DbExceptionTestConfig configuration class
-    	// used by @ContextConfiguration annotation above.
-        assertThrows(RewardDataAccessException.class, () -> {
-            repository.findByCreditCard("1234123412341234");
-        });
+	@Test
+	@CaptureSystemOutput
+	public void testReportException(OutputCapture capture) {
+		assertThrows(RewardDataAccessException.class, () -> {
+			repository.findByCreditCard("1234123412341234");
+		});
 
-        // TODO-12: (Optional) Validate our AOP is working.
-        //
-        // - An error message should now be logged to the console as a warning
-        // - Save all your work and run this test - it should pass with a warning
-        //   message on the console AND the console output assertion (below)
-        //   should succeed.
+		// The error message should have been logged to the console as a warning
+		assertThat(capture.toString(), containsString(DBExceptionHandlingAspect.EMAIL_FAILURE_MSG));
+	}
 
-        if (TestConstants.CHECK_CONSOLE_OUTPUT) {
-            assertThat(capture.toString(), containsString(DBExceptionHandlingAspect.EMAIL_FAILURE_MSG));
-        }
-    }
 }
