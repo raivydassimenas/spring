d4e0944799e2f60e50b4a48582523525ec359918
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-09-16 18:37:30 +0300

    Complete 03 and 04 todos of JdbcTemplate

--- lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
+++ lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
@@ -2,6 +2,7 @@ package rewards.internal.restaurant;
 
 import common.money.Percentage;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.Dining;
 import rewards.internal.account.Account;
 
@@ -39,9 +40,11 @@ import java.sql.SQLException;
 public class JdbcRestaurantRepository implements RestaurantRepository {
 
 	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 
 	public JdbcRestaurantRepository(DataSource dataSource) {
 		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public Restaurant findByMerchantNumber(String merchantNumber) {
@@ -49,17 +52,19 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 				+ " from T_RESTAURANT where MERCHANT_NUMBER = ?";
 		Restaurant restaurant = null;
 
-		try (Connection conn = dataSource.getConnection();
-			 PreparedStatement ps = conn.prepareStatement(sql) ){
-			ps.setString(1, merchantNumber);
-			ResultSet rs = ps.executeQuery();
-			advanceToNextRow(rs);
-			restaurant = mapRestaurant(rs);
-		} catch (SQLException e) {
-			throw new RuntimeException("SQL exception occurred finding by merchant number", e);
-		}
+//		try (Connection conn = dataSource.getConnection();
+//			 PreparedStatement ps = conn.prepareStatement(sql) ){
+//			ps.setString(1, merchantNumber);
+//			ResultSet rs = ps.executeQuery();
+//			advanceToNextRow(rs);
+//			restaurant = mapRestaurant(rs);
+//		} catch (SQLException e) {
+//			throw new RuntimeException("SQL exception occurred finding by merchant number", e);
+//		}
+//
+//		return restaurant;
 
-		return restaurant;
+		return jdbcTemplate.queryForObject(sql, new Object[] {merchantNumber}, (rs, rowNum) -> mapRestaurant(rs));
 	}
 
 	/**
